cbabc7327fb64e8c688966ee9fb7c8186f312f81
Jimmy Bourassa <jimmy.bourassa@hooktstudios.com>
2011-12-10 23:22:43 -0500

    Pushing to the queue now works

--- sse/views.py
+++ sse/views.py
@@ -1,7 +1,9 @@
-import datetime, time
+import datetime, time, zmq, json
 from pyramid.view import view_config
 from pyramid.response import Response
 
+SOCK = 'ipc:///tmp/zmq.test'
+context = zmq.Context()
 
 @view_config(route_name='home', renderer='/home.mako')
 def home(request):
@@ -15,6 +17,16 @@ def events(request):
     response.app_iter = time_generator()
     return response
     
+@view_config(route_name = 'push')
+def push(request):
+    msg = request.json_body['message']
+
+    socket = context.socket(zmq.PUB)
+    socket.bind(SOCK)
+    socket.send(msg.encode('utf-8'))
+
+    return Response()
+
 def time_generator():
     while True:
         yield "data: %s\n\n" % str(datetime.datetime.now())
